cf46e055a52e0826bd5c5991bceaf2cfdb02bb03
seeewo4kin <sewo4kin321@gmail.com>
2025-08-10 09:51:29 +0300

    jdbc fix

--- src/main/java/jm/task/core/jdbc/service/UserService.java
+++ src/main/java/jm/task/core/jdbc/service/UserService.java
@@ -1,10 +1,14 @@
 package jm.task.core.jdbc.service;
 
+import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.dao.UserDaoJDBCImpl;
 import jm.task.core.jdbc.model.User;
 
 import java.util.List;
 
 public interface UserService {
+    UserDaoJDBCImpl userDaoJDBC = new UserDaoJDBCImpl();
+    UserDaoHibernateImpl userDaoHibernate = new UserDaoHibernateImpl();
     void createUsersTable();
 
     void dropUsersTable();
